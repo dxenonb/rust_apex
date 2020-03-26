9e45f1bd1647de0dd5340a9ad39511c7e8c730ae
dxb <drdxbme@gmail.com>
2020-03-25 23:29:12 -0400

    Display parse results

--- src/main.rs
+++ src/main.rs
@@ -10,28 +10,52 @@ extern crate pest_derive;
 struct ApexParser;
 
 fn main() {
+
+    let mut errors = Vec::new();
+    let mut success = 0;
+
     for entry in read_dir("examples").unwrap() {
         let entry = entry.unwrap();
         let path = &entry.path();
         if path.is_file() {
             let contents = read_to_string(&path).unwrap();
             println!("Parsing file: {:?}", &path);
-            display_parse(&contents);
+            if let Err(err) = display_parse(&contents) {
+                errors.push(err);
+            } else {
+                success += 1;
+            }
         }
+    }
+
+    // TODO: oops forgot to track file name
+    for error in &errors {
+        println!("Error: {}", error);
+    }
+
+    let total = success + errors.len();
+
+    println!("Parsed {} files.", total);
+    println!("\tparsing: {}", success);
+    println!("\tfailing: {}", errors.len());
+
+    if !errors.is_empty() {
+        println!("\nSee above for errors.");
     }
 }
 
-fn display_parse(input: &str) {
-    let pairs = match ApexParser::parse(Rule::class, input) {
+fn display_parse(input: &str) -> Result<(), String> {
+    let _pairs = match ApexParser::parse(Rule::class, input) {
         Ok(r) => r,
         Err(err) => {
-            println!("\t{}", err);
-            return;
+            return Err(format!("{}", err));
         }
     };
-    for pair in pairs {
-        println!("\tPair: {:?}", pair);
-    }
+    // TODO: we probably don't actually want to print all this... or do we?
+    // for pair in pairs {
+    //     println!("\tPair: {:?}", pair);
+    // }
+    Ok(())
 }
 
 #[cfg(test)]
