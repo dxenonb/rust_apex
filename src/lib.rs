88565f908a6840fdfc9243ede4e6d0a8849e52d6
dxb <drdxbme@gmail.com>
2020-03-28 00:28:14 -0400

    Parse for loops

--- src/lib.rs
+++ src/lib.rs
@@ -30,7 +30,7 @@ mod test {
         };
     }
 
-    // TODO: extends, implements, nested classes
+    // TODO: extends, implements, nested classes, for loops
 
     // TODO: Upgrade these to use the new "matches!" macro
 
